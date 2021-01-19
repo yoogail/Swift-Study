f9547ec8c9ca5b3a216d8dc1888592cd93f9ea6f
yoogail <yoogail105@gmail.com>
2021-01-19 23:46:33 +0900

    working with frames

--- S4_SwooshApp/app-swoosh/app-swoosh/ViewController.swift
+++ S4_SwooshApp/app-swoosh/app-swoosh/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+
+        bgImage.frame = view.frame; //size = viewㄹ
+    
     }
 
 
