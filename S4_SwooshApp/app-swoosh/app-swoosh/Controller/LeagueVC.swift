ce25c3a2373e4e81c860b90e32cf93fa76ed8dbf
yoogail <yoogail105@gmail.com>
2021-01-22 04:50:20 +0900

    Programmatic segues

--- S4_SwooshApp/app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ S4_SwooshApp/app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,12 +9,18 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func onNextTapped(_ sender: Any) {
+            
+            performSegue(withIdentifier: "skillVCSegue", sender: self)
+        }
 
     /*
     // MARK: - Navigation
