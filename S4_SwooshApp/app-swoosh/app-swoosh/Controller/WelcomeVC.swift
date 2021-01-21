666067256cf760978895cbda1e2a18e988a3bd3f
yoogail <yoogail105@gmail.com>
2021-01-22 04:05:33 +0900

    Debugging setValue forUndefinedKey

--- S4_SwooshApp/app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ S4_SwooshApp/app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -9,9 +9,7 @@ import UIKit
 
 class WelcomeVC: UIViewController {
 
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImage: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     
