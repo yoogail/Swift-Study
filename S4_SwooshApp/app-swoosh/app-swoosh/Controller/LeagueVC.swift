6111d339128c0f192815253a290127c1c86d445b
yoogail <yoogail105@gmail.com>
2021-01-22 14:23:49 +0900

    IBActions and data models

--- S4_SwooshApp/app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ S4_SwooshApp/app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+
+    @IBOutlet weak var nextBtn: BorderButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
+        
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
@@ -22,11 +26,27 @@ class LeagueVC: UIViewController {
             performSegue(withIdentifier: "skillVCSegue", sender: self)
         }
 
-    /*
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+
+    @IBAction func onCoedTapped(_ sender: Any) {
+        player.desiredLegue = "coed"
+    }
+
+    func selectLeague(leagueType: String) {
+        player.desiredLegue = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+        /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    koverride func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
---
+++ S4_SwooshApp/app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by minjoo on 2021/01/22.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLegue: String?
+    var selectedSkillLevel: String?
+    
+}
+
