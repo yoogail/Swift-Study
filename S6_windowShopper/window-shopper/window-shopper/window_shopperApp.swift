7b3593f6dcbbc6f811bf2e906c190331a29dbc12
yoogail <yoogail105@gmail.com>
2021-02-07 02:43:19 +0900

    new dir

---
+++ S6_windowShopper/window-shopper/window-shopper/window_shopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  window_shopperApp.swift
+//  window-shopper
+//
+//  Created by minjoo on 2021/02/07.
+//
+
+import SwiftUI
+
+@main
+struct window_shopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
