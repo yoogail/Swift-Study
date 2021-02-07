90b58d652c0f52502b37742ab04fb0a5b986599a
yoogail <yoogail105@gmail.com>
2021-02-07 18:14:38 +0900

    make logo

---
+++ C2_Fruits/C2_Fruits/C2_FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  C2_FruitsApp.swift
+//  C2_Fruits
+//
+//  Created by minjoo on 2021/02/07.
+//
+
+import SwiftUI
+
+@main
+struct C2_FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C2_Fruits/C2_Fruits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  C2_Fruits
+//
+//  Created by minjoo on 2021/02/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
