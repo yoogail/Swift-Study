f6f781be1bb3c1e279f76ac7802629ad82e453fd
yoogail <yoogail105@gmail.com>
2021-02-07 17:50:04 +0900

    new

---
+++ C2_Fruits/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by minjoo on 2021/02/07.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
